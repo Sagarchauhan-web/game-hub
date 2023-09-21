9f765ef6cf5fe80ccdf54fcd40388bdace1ea34c
Sagar <Sagarchawan4646@gmail.com>
2023-09-21 19:05:33 +0530

    Filter games by genre

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { Grid, GridItem, Show } from '@chakra-ui/react';
+import { useState } from 'react';
 import GameGrid from './components/GameGrid';
 import GenreList from './components/GenreList';
 import NavBar from './components/NavBar';
+import { Genre } from './hooks/useGenres';
 
 function App() {
+  const [selectGenre, setSelectGenre] = useState<Genre | null>(null);
+  console.log(selectGenre, 'selectGenre');
+
   return (
     <Grid
       templateAreas={{
@@ -20,11 +25,11 @@ function App() {
       </GridItem>
       <Show above='lg'>
         <GridItem area='aside' paddingX={5}>
-          <GenreList />
+          <GenreList onSelectGenre={setSelectGenre} />
         </GridItem>
       </Show>
       <GridItem area='main'>
-        <GameGrid />
+        <GameGrid selectedGenre={selectGenre} />
       </GridItem>
     </Grid>
   );
