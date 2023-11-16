dce4b33bca16a0bb3a015688c48b51bdfd67ecbf
Sagar <Sagarchawan4646@gmail.com>
2023-11-16 20:49:38 +0530

    Setup routing

---
+++ src/routes.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from 'react-router-dom';
+import GameDetailsPage from './pages/GameDetailsPage';
+import HomePage from './pages/HomePage';
+import Layout from './pages/layout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'games/:id', element: <GameDetailsPage /> },
+    ],
+  },
+]);
+
+export default router;
